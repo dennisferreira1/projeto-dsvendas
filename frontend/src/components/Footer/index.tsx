642ad460a296f306a9e36ec07c75a37e2a889472
dennisferreira1 <dennisferreira1@hotmail.com>
2021-09-11 11:39:03 -0300

    Home, navegação

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,14 +1,12 @@
 const Footer = () => {
     return (
-        <>
-            <footer className="footer mt-auto py-3 bg-dark">
-                <div className="container">
-                    <p className="text-light">App desenvolvido por <a href="https://github.com/dennisferreira1" target="_blank" rel="noreferrer">Dennis Ferreira</a></p>
-                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
-                </div>
-            </footer>
-        </>
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/dennisferreira1" target="_blank" rel="noreferrer">Dennis Ferreira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
     );
 }
 
